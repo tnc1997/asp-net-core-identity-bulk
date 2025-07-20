c4897ab004340a8a641f44d3cd3c78c98498a434
Thomas Clark <tnc1997@outlook.com>
2025-07-20 10:37:34 +0100

    style(entity-framework-core): add line breaks to parameters

--- src/Tnc1997.AspNetCore.Identity.Bulk.EntityFrameworkCore/BulkUserStore.cs
+++ src/Tnc1997.AspNetCore.Identity.Bulk.EntityFrameworkCore/BulkUserStore.cs
@@ -12,14 +12,18 @@ namespace Tnc1997.AspNetCore.Identity.Bulk.EntityFrameworkCore;
 /// Represents a new instance of a persistence store for users, using the default implementation
 /// of <see cref="IdentityUser{TKey}"/> with a string as a primary key.
 /// </summary>
-public class BulkUserStore(DbContext context, IdentityErrorDescriber? describer = null)
+public class BulkUserStore(
+    DbContext context,
+    IdentityErrorDescriber? describer = null)
     : BulkUserStore<IdentityUser<string>>(context, describer);
 
 /// <summary>
 /// Creates a new instance of a persistence store for the specified user type.
 /// </summary>
 /// <typeparam name="TUser">The type representing a user.</typeparam>
-public class BulkUserStore<TUser>(DbContext context, IdentityErrorDescriber? describer = null)
+public class BulkUserStore<TUser>(
+    DbContext context,
+    IdentityErrorDescriber? describer = null)
     : BulkUserStore<TUser, IdentityRole, DbContext, string>(context, describer)
     where TUser : IdentityUser<string>;
 
@@ -29,7 +33,9 @@ public class BulkUserStore<TUser>(DbContext context, IdentityErrorDescriber? des
 /// <typeparam name="TUser">The type representing a user.</typeparam>
 /// <typeparam name="TRole">The type representing a role.</typeparam>
 /// <typeparam name="TContext">The type of the data context class used to access the store.</typeparam>
-public class BulkUserStore<TUser, TRole, TContext>(TContext context, IdentityErrorDescriber? describer = null)
+public class BulkUserStore<TUser, TRole, TContext>(
+    TContext context,
+    IdentityErrorDescriber? describer = null)
     : BulkUserStore<TUser, TRole, TContext, string>(context, describer)
     where TUser : IdentityUser<string>
     where TRole : IdentityRole<string>
@@ -42,7 +48,9 @@ public class BulkUserStore<TUser, TRole, TContext>(TContext context, IdentityErr
 /// <typeparam name="TRole">The type representing a role.</typeparam>
 /// <typeparam name="TContext">The type of the data context class used to access the store.</typeparam>
 /// <typeparam name="TKey">The type of the primary key for a role.</typeparam>
-public class BulkUserStore<TUser, TRole, TContext, TKey>(TContext context, IdentityErrorDescriber? describer = null)
+public class BulkUserStore<TUser, TRole, TContext, TKey>(
+    TContext context,
+    IdentityErrorDescriber? describer = null)
     : BulkUserStore<TUser, TRole, TContext, TKey, IdentityUserClaim<TKey>, IdentityUserRole<TKey>, IdentityUserLogin<TKey>, IdentityUserToken<TKey>, IdentityRoleClaim<TKey>>(context, describer)
     where TUser : IdentityUser<TKey>
     where TRole : IdentityRole<TKey>
@@ -61,7 +69,9 @@ public class BulkUserStore<TUser, TRole, TContext, TKey>(TContext context, Ident
 /// <typeparam name="TUserLogin">The type representing a user external login.</typeparam>
 /// <typeparam name="TUserToken">The type representing a user token.</typeparam>
 /// <typeparam name="TRoleClaim">The type representing a role claim.</typeparam>
-public class BulkUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, TUserLogin, TUserToken, TRoleClaim>(TContext context, IdentityErrorDescriber? describer = null)
+public class BulkUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, TUserLogin, TUserToken, TRoleClaim>(
+    TContext context,
+    IdentityErrorDescriber? describer = null)
     : IBulkUserStore<TUser>
     where TUser : IdentityUser<TKey>
     where TRole : IdentityRole<TKey>
@@ -77,7 +87,9 @@ public class BulkUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, 
 
     private bool _disposed;
 
-    public async Task<IEnumerable<IdentityResult>> CreateAsync(IEnumerable<TUser> users, CancellationToken cancellationToken)
+    public async Task<IEnumerable<IdentityResult>> CreateAsync(
+        IEnumerable<TUser> users,
+        CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
         ObjectDisposedException.ThrowIf(_disposed, this);
@@ -90,7 +102,9 @@ public class BulkUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, 
         return Enumerable.Repeat(IdentityResult.Success, users.Count());
     }
 
-    public async Task<IEnumerable<IdentityResult>> DeleteAsync(IEnumerable<TUser> users, CancellationToken cancellationToken)
+    public async Task<IEnumerable<IdentityResult>> DeleteAsync(
+        IEnumerable<TUser> users,
+        CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
         ObjectDisposedException.ThrowIf(_disposed, this);
@@ -110,7 +124,9 @@ public class BulkUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, 
         return Enumerable.Repeat(IdentityResult.Success, users.Count());
     }
 
-    public async Task<IEnumerable<IdentityResult>> UpdateAsync(IEnumerable<TUser> users, CancellationToken cancellationToken)
+    public async Task<IEnumerable<IdentityResult>> UpdateAsync(
+        IEnumerable<TUser> users,
+        CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
         ObjectDisposedException.ThrowIf(_disposed, this);
