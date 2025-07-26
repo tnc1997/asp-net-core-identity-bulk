c00a113ba93b9a3be9a0a41afdcb4b5d00ba6b30
Thomas Clark <tnc1997@outlook.com>
2025-07-26 15:39:08 +0100

    test(entity-framework-core): add get user names

--- test/Tnc1997.AspNetCore.Identity.Bulk.EntityFrameworkCore.Tests/BulkUserStoreTests.cs
+++ test/Tnc1997.AspNetCore.Identity.Bulk.EntityFrameworkCore.Tests/BulkUserStoreTests.cs
@@ -86,6 +86,23 @@ public class BulkUserStoreTests
     }
 
     [Test]
+    public async Task GetUserNamesAsync()
+    {
+        // Arrange
+        using var store = new BulkUserStore(_context);
+
+        var expected = new List<string> { "alice", "bob" };
+
+        var users = expected.Select(userName => new IdentityUser { UserName = userName });
+
+        // Act
+        var actual = await store.GetUserNamesAsync(users, CancellationToken.None);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
     public async Task UpdateAsync()
     {
         // Arrange
