eb71fa3df636dcc0b7c407fdc10c3a52f8d8caa4
Thomas Clark <tnc1997@outlook.com>
2025-08-01 21:27:50 +0100

    test: add bulk user validator

---
+++ test/Tnc1997.AspNetCore.Identity.Bulk.Tests/IdentityErrorComparer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tnc1997.AspNetCore.Identity.Bulk.Tests;
+
+public class IdentityErrorComparer : IEqualityComparer<IdentityError>
+{
+    public bool Equals(IdentityError? x, IdentityError? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null)
+        {
+            return false;
+        }
+
+        if (y is null)
+        {
+            return false;
+        }
+
+        if (x.GetType() != y.GetType())
+        {
+            return false;
+        }
+
+        return x.Code == y.Code;
+    }
+
+    public int GetHashCode(IdentityError obj)
+    {
+        return obj.Code.GetHashCode();
+    }
+}
---
+++ test/Tnc1997.AspNetCore.Identity.Bulk.Tests/IdentityResultComparer.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tnc1997.AspNetCore.Identity.Bulk.Tests;
+
+public class IdentityResultComparer : IEqualityComparer<IdentityResult>
+{
+    private readonly IdentityErrorComparer _comparer = new();
+
+    public bool Equals(IdentityResult? x, IdentityResult? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null)
+        {
+            return false;
+        }
+
+        if (y is null)
+        {
+            return false;
+        }
+
+        if (x.GetType() != y.GetType())
+        {
+            return false;
+        }
+
+        return Enumerable.SequenceEqual(x.Errors, y.Errors, _comparer);
+    }
+
+    public int GetHashCode(IdentityResult obj)
+    {
+        return obj.Errors.GetHashCode();
+    }
+}
